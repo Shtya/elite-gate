bbc2dd3fa90788b100780ee2771b2058742c407e
amagdykhalil <am2592379@gmai.com>
2025-09-24 14:10:39 +0300

    Eit footer right

--- src/components/shared/Footer/FooterRights.tsx
+++ src/components/shared/Footer/FooterRights.tsx
@@ -7,7 +7,9 @@ export default function FooterRights() {
 
     return (
         <p className="m-0 text-center lg:text-start">
-
+            جميع الحقوق محفوظة © {currentYear}.
+            <span className="text-tertiary"> مدعوم من </span>
+            <Link href="https://dukanomar.com/" target='_blank' className="text-tertiary hover:underline">Elite Gate</Link>.
         </p>
     );
 }
